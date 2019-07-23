6805053bfeaa7af4ebfe112ecf704557c29548b8
kolapo <kolapoajijola@yahoo.com>
2019-07-22 23:30:07 -0400

    adding this to git initial config

---
+++ roles/php/files/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title> welcome to drealkaypee's aws</title>
+    </head>
+    <body>
+      <h1><?php echo "This is my PHP config website" ?></h1>
+    </body>
+    </html>
