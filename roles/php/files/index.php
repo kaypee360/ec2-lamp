e9af7b116ba2f7081151684e0330310dbc292114
kolapo <kolapoajijola@yahoo.com>
2019-07-23 02:36:03 -0400

    send me the location update for cron

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -4,6 +4,6 @@
         <title> welcome to the second drealkaypee's aws</title>
     </head>
     <body>
-      <h1><?php echo "This is my second PHP config website" ?></h1>
+      <h1><?php echo "Make you send me the Location for the cron job" ?></h1>
     </body>
     </html>
