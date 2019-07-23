554d99694251053a17c7dbeff546138b4bfe1580
kaypee360 <doyinajijola@yahoo.com>
2019-07-23 02:10:11 -0400

    Update index.php

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
     <html>
     <head>
-        <title> welcome to drealkaypee's aws</title>
+        <title> welcome to the second drealkaypee's aws</title>
     </head>
     <body>
-      <h1><?php echo "This is my PHP config website" ?></h1>
+      <h1><?php echo "This is my second PHP config website" ?></h1>
     </body>
     </html>
